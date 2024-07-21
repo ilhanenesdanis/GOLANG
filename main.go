d6766ad99ebfbfb59c11425ce3cdb93ad1dc178d
ilhan enes daniş <danisenes3@gmail.com>
2024-07-21 23:26:57 +0300

    variables

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello World")
+	/*
+				var message string = "Merhaba go!"
+
+				fmt.Println(message)
+
+				var message = "Merhaba Go lang"
+
+				fmt.Println(message)
+
+				var a, b, c int
+
+				a = 1
+				b = 34
+				c = 3
+
+				fmt.Println(a)
+				fmt.Println(b)
+				fmt.Println(c)
+
+				var message = "Hello World"
+
+			    var a, b, c = 3, true, 4.5
+
+			    fmt.Println(message, a, b, c)
+
+		u := 55
+
+		fmt.Println(u)
+	*/
+
 }
