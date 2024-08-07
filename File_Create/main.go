cb8577c7c2cb59b673892e81fc0db6e5e79de721
ilhan enes daniş <danisenes3@gmail.com>
2024-08-07 21:42:29 +0300

    fileCreate

---
+++ File_Create/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	newFile *os.File
+	err     error
+)
+
+func main() {
+
+	newFile, err = os.Create("demo.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
